5426faf98ac39840c81a45514fc0aced83dece01
Joseph Birr-Pixton <jpixton@gmail.com>
2015-01-20 02:14:48 +0000

    testutil: don't overflow buffer. unhelpful.

--- src/testutil.h
+++ src/testutil.h
@@ -21,9 +21,11 @@ static inline size_t unhex(uint8_t *buf, size_t len, const char *str)
   size_t used = 0;
 
   assert(strlen(str) % 2 == 0);
+  assert(strlen(str) / 2 <= len);
 
   while (*str)
   {
+    assert(len);
     *buf = unhex_chr(str[0]) << 4 | unhex_chr(str[1]);
     buf++;
     used++;
