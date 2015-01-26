495dc24701a723ab7cdd7eea9202bc0c20d07c32
Joseph Birr-Pixton <jpixton@gmail.com>
2015-01-26 21:45:50 +0000

    stm32f3 is a M4F part, not M1.

--- src/arm/report.py
+++ src/arm/report.py
@@ -1,4 +1,4 @@
-archs = 'stm32f0 stm32f3'.split()
+archs = 'stm32f0 stm32f1 stm32f3'.split()
 tests = """
 aes128block_test
 aes256block_test
@@ -12,6 +12,12 @@ salsa20_test
 chacha20_test
 do_nothing
 """.split()
+
+arch_names = dict(
+        stm32f0 = 'Cortex-M0',
+        stm32f1 = 'Cortex-M3',
+        stm32f3 = 'Cortex-M4F'
+        )
 
 base_test = 'do_nothing'
 
@@ -47,6 +53,21 @@ def extract(arch, test):
             stack_usage = stack_usage
             )
 
+def print_table(rows):
+    header, rows = rows[0], rows[1:]
+    assert not [True for r in rows if len(r) != len(header)]
+    widths = []
+    for i, h in enumerate(header):
+        widths.append(max([len(h)] + [len(r[i]) for r in rows]))
+
+    def print_row(row):
+        print ' | '.join(c + (' ' * (widths[i] - len(c))) for i, c in enumerate(row))
+    
+    print_row(header)
+    print_row(['-' * w for w in widths])
+    for r in rows:
+        print_row(r)
+
 results = {}
 
 for arch in archs:
@@ -77,31 +98,76 @@ def print_aes(label, block_test, sched_test):
     print '    * **Code size**: %dB' % block_test['code_size']
     print
 
+def tabulate_aes(arch, block_result, sched_result, table = None):
+    if table is None:
+        table = []
+        table.append((
+            'Core',
+            'Cycles (key schedule + block)',
+            'Cycles (key schedule)',
+            'Cycles (block)',
+            'Stack',
+            'Code size'
+            ))
+
+    table.append(
+            (
+                arch_names[arch],
+                '%d' % block_result['cycle_count'],
+                '%d' % sched_result['cycle_count'],
+                '%d' % (block_result['cycle_count'] - sched_result['cycle_count']),
+                '%dB' % block_result['stack_usage'],
+                '%dB' % block_result['code_size']
+            ))
+
+    return table
+
 def print_std(result):
     print """* **Cycles**: %(cycle_count)d
 * **Stack**: %(stack_usage)dB
 * **Code size**: %(code_size)dB
 """ % result
 
-for arch in results.keys():
-    print '##', 'AES (%s)' % arch
-    print_aes('128 bit key', results[arch]['aes128block_test'], results[arch]['aes128sched_test'])
-    print_aes('256 bit key', results[arch]['aes256block_test'], results[arch]['aes256sched_test'])
-    
-    print '## AES128-GCM (%s)' % arch
-    print_std(results[arch]['aes128gcm_test'])
-    
-    print '## AES128-EAX (%s)' % arch
-    print_std(results[arch]['aes128eax_test'])
-    
-    print '## Chacha20 (%s)' % arch
-    print_std(results[arch]['chacha20_test'])
+def tabulate_std(arch, result, table = None):
+    if table is None:
+        table = []
+        table.append(('Core', 'Cycles', 'Stack', 'Code size'))
 
-    print '## Salsa20 (%s)' % arch
-    print_std(results[arch]['salsa20_test'])
+    table.append(
+            (
+                arch_names[arch],
+                '%d' % result['cycle_count'],
+                '%dB' % result['stack_usage'],
+                '%dB' % result['code_size']
+            ))
 
-    print '## SHA256 (%s)' % arch
-    print_std(results[arch]['hashtest_sha256'])
-    
-    print '## SHA512 (%s)' % arch
-    print_std(results[arch]['hashtest_sha512'])
+    return table
+
+def tabulate(mktab):
+    table = None
+    for arch in archs:
+        if arch not in results:
+            continue
+        table = mktab(arch, table)
+    print_table(table)
+
+print '##', 'AES (128-bit key)'
+tabulate(lambda arch, table: tabulate_aes(arch, results[arch]['aes128block_test'], results[arch]['aes128sched_test'], table))
+print
+
+print '##', 'AES (256-bit key)'
+tabulate(lambda arch, table: tabulate_aes(arch, results[arch]['aes256block_test'], results[arch]['aes256sched_test'], table))
+print
+
+def do_table(title, test):
+    print '##', title
+    tabulate(lambda arch, table: tabulate_std(arch, results[arch][test], table))
+    print
+
+do_table('AES128-GCM', 'aes128gcm_test')
+do_table('AES128-EAX', 'aes128eax_test')
+do_table('ChaCha20', 'chacha20_test')
+do_table('Salsa20', 'salsa20_test')
+do_table('SHA256', 'hashtest_sha256')
+do_table('SHA512', 'hashtest_sha512')
+
